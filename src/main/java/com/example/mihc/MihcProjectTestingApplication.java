6d3678c7be19a702dc8185d65bcfe4ea369997db
Muthia <68548653+muthiaqrrta@users.noreply.github.com>
2023-01-23 15:08:15 +0700

    initial commit

---
+++ src/main/java/com/example/mihc/MihcProjectTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mihc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MihcProjectTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MihcProjectTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mihc/MihcProjectTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mihc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MihcProjectTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
